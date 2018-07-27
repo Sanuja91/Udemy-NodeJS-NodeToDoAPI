7e13e6862669f7bd8ff125c910c087ffa53ee72b
Sanuja-Laptop <sanujac@live.com>
2018-07-27 23:25:21 +0530

    Bug fix

--- server/server.js
+++ server/server.js
@@ -28,14 +28,14 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
     let id = req.params.id
     if (!ObjectID.isValid(id))
-        return res.send(`Invalid ID ${id}`)
+        return res.status(404).send(`Invalid ID ${id}`)
 
     Todo.findById(id)
         .then(todo => {
             if (!todo)
-                return res.status(400).send('ID not found')
+                return res.status(404).send('ID not found')
             console.log('Todo by ID', todo)
-            res.send(`Found ID ${JSON.stringify(req.params)}`)
+            res.send({todo})
         })
         .catch(err => {
             console.log('Error', err)
