bc9a3e5e82a91e9f443b625c90a4e2ac979e47ae
Sanuja-Laptop <sanujac@live.com>
2018-07-27 15:27:52 +0530

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -17,6 +17,13 @@ app.post('/todos', (req, res) => {
         .catch(err => { res.status(400).send(err) })
 })
 
+app.get('/todos', (req, res) => {
+    Todo.find()
+        .then(
+            todos => { res.send({ todos }) },
+            err => { res.status(400).send(err) })
+})
+
 app.listen(3000, () => {
     console.log('Started on port 3000')
 })
